5edd0f7987306fe9112bc3deed7daac640a9cabc
nathankurz <nathankurz91@gmail.com>
2021-01-22 16:15:11 -0500

    Initial commit

---
+++ src/AreaOfCircle.java
@@ -0,0 +1,19 @@
+//Author: Halena Wetton
+//Date: January 22, 2020
+//File: AreaOfCircle.java
+
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+
+        //Declare variables
+        double pi = 3.14;
+        int radius = 5;
+        double area = 0;
+        area = pi * (radius * radius);
+
+        //Print statements
+        System.out.println("Radius of the circle is : " + radius);
+        System.out.println("The area of the circle is : " + area);
+    }
+}
